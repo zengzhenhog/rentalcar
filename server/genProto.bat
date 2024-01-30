7770699a9231ceba65fe09629b7093ab115a02aa
曾振鸿 <1004105806@qq.com>
2024-01-30 21:23:17 +0800

    U -

--- server/genProto.bat
+++ server/genProto.bat
@@ -1,8 +1,17 @@
-set PROTO_PATH=.\rental\api
-set GO_OUT_PATH=.\rental\api\gen\v1
+@echo off
+setlocal
 
+call :createProto "rental"
+call :createProto "auth"
+
+exit /b
+
+:createProto
+set PROTO_PATH=.\%~1\api
+set GO_OUT_PATH=.\%~1\api\gen\v1
 mkdir %GO_OUT_PATH%
 
-protoc -I=%PROTO_PATH% --go_out=paths=source_relative:%GO_OUT_PATH% rental.proto
-protoc -I=%PROTO_PATH% --go-grpc_opt require_unimplemented_servers=false --go-grpc_out=paths=source_relative:%GO_OUT_PATH% rental.proto
-protoc -I=%PROTO_PATH% --grpc-gateway_out=paths=source_relative,grpc_api_configuration=%PROTO_PATH%\rental.yaml:%GO_OUT_PATH% rental.proto
+protoc -I=%PROTO_PATH% --go_out=paths=source_relative:%GO_OUT_PATH% %~1.proto
+protoc -I=%PROTO_PATH% --go-grpc_opt require_unimplemented_servers=false --go-grpc_out=paths=source_relative:%GO_OUT_PATH% %~1.proto
+protoc -I=%PROTO_PATH% --grpc-gateway_out=paths=source_relative,grpc_api_configuration=%PROTO_PATH%\%~1.yaml:%GO_OUT_PATH% %~1.proto
+exit /b
--- server/go.mod
+++ server/go.mod
@@ -12,7 +12,7 @@ require (
 	go.mongodb.org/mongo-driver v1.11.4
 	go.uber.org/zap v1.24.0
 	google.golang.org/grpc v1.55.0
-	google.golang.org/protobuf v1.30.0
+	google.golang.org/protobuf v1.31.0
 )
 
 require (
